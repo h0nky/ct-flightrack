2c6cee7840ed3cba5faab177bb455a7c126563b5
Eugene Golovnov <eugene.golovnov@whykingz.com>
2021-02-26 00:50:54 +0200

    initial commit / add google-map-reacts / add styled-components

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'cartrack-logo.svg'} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Cartrack's Front-End Tech Challenge.
+        </p>
+        <p>
+          Happy Coding!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
